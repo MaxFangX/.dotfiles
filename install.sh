9a151bb104ab0c293783ca7b4d640efabc54cc96
Max Fang <maxfangx@gmail.com>
2017-07-03 12:17:35 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
+# Install Cmake so that installing submodules works
+brew install cmake
+
+# Install Node
+brew install node
+
 git submodule update --init --recursive
 
 # YouCompleteMe
@@ -7,14 +13,16 @@ popd
 
 # ZSH
 # Instructions here: http://sourabhbajaj.com/mac-setup/iTerm/zsh.html
-# brew install zsh zsh-completions
-# curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
-# chsh -s /usr/local/bin/zsh
+brew install zsh zsh-completions
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+chsh -s /usr/local/bin/zsh
+chsh -s /bin/zsh
 
-### Typecript
+# Oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Tsuquyomi https://github.com/Quramy/tsuquyomi
-# git clone https://github.com/Shougo/vimproc.vim.git ~/.vim/bundle/vimproc.vim
+git clone https://github.com/Shougo/vimproc.vim.git ~/.vim/bundle/vimproc.vim
 pushd ~/.vim/bundle/vimproc.vim
 make
 popd
@@ -25,3 +33,20 @@ git clone https://github.com/Quramy/tsuquyomi.git ~/.vim/bundle/tsuquyomi
 pushd ~/.vim/bundle/tern_for_vim
 npm install
 popd
+
+# Install thefuck
+brew install thefuck
+
+# Install the silver searcher
+brew install the_silver_searcher
+
+# Install vim powerline fonts
+# Instructions here: https://github.com/powerline/fonts
+git clone https://github.com/powerline/fonts.git
+cd fonts
+./install.sh
+cd ..
+rm -rf fonts
+
+# Install vim plugins
+vim +PluginInstall +qall
