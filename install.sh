cc3d9a36a1ee2f960d7d7420a388f6ae7814f751
Max Fang <maxfangx@gmail.com>
2017-07-07 16:28:57 -0700

    install: cash, python, chrome two finger drag

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
+###########################
+# System
+###########################
+
 # Install brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install brew cask
+brew tap caskroom/cask
+
+# Java
+brew cask install java
 
 # Install gpg
 brew install gpg
@@ -15,22 +25,28 @@ brew install cmake
 # Install Node
 brew install node
 
+# Install Python
+brew install python
+
+# Install pip and virtualenv
+sudo easy_install pip
+pip install virtualenv
+
+# Install PostGreSQL
+brew install postgresql
+
+###########################
+# VIM
+###########################
+
+# Update submodules
 git submodule update --init --recursive
 
 # YouCompleteMe
+# http://www.nyayapati.com/srao/2014/12/installing-homebrew-macvim-with-youcompleteme-on-yosemite/
 pushd ~/.vim/bundle/YouCompleteMe
 ./install.py --clang-completer
 popd
-
-# ZSH
-# Instructions here: http://sourabhbajaj.com/mac-setup/iTerm/zsh.html
-brew install zsh zsh-completions
-curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
-chsh -s /usr/local/bin/zsh
-chsh -s /bin/zsh
-
-# Oh my zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Tsuquyomi https://github.com/Quramy/tsuquyomi
 git clone https://github.com/Shougo/vimproc.vim.git ~/.vim/bundle/vimproc.vim
@@ -45,6 +61,31 @@ pushd ~/.vim/bundle/tern_for_vim
 npm install
 popd
 
+# Install vim powerline fonts
+# Instructions here: https://github.com/powerline/fonts
+git clone https://github.com/powerline/fonts.git
+cd fonts
+./install.sh
+cd ..
+rm -rf fonts
+
+# Install Vundle plugins
+vim +PluginInstall +qall
+
+###########################
+# Command line tools
+###########################
+
+# ZSH
+# Instructions here: http://sourabhbajaj.com/mac-setup/iTerm/zsh.html
+brew install zsh zsh-completions
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+chsh -s /usr/local/bin/zsh
+chsh -s /bin/zsh
+
+# Oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 # Install thefuck
 brew install thefuck
 
@@ -57,13 +98,10 @@ brew install tldr
 # Tree
 brew install tree
 
-# Install vim powerline fonts
-# Instructions here: https://github.com/powerline/fonts
-git clone https://github.com/powerline/fonts.git
-cd fonts
-./install.sh
-cd ..
-rm -rf fonts
+###########################
+# Misc
+###########################
 
-# Install Vundle plugins
-vim +PluginInstall +qall
+# Disable Chrome's two finger drag
+# https://apple.stackexchange.com/questions/21236/how-do-i-disable-chromes-two-finger-back-forward-navigation
+defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool FALSE
