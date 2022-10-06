5b5080dbf1396cfb078a89d6369593b7f9987e49
Max Fang <protonmail.com@maxfa.ng>
2022-10-06 14:08:17 -0700

    scripts: Update for cargo / coc.nvim configs

--- scripts/from-dotfiles.sh
+++ scripts/from-dotfiles.sh
@@ -7,6 +7,11 @@ cp ~/.dotfiles/gitconfig ~/.gitconfig
 # Neovim
 mkdir -p ~/.config/nvim
 cp ~/.dotfiles/nvim/init.vim ~/.config/nvim
+cp ~/.dotfiles/nvim/coc-settings.json ~/.config/nvim
 rm -rf ~/.config/nvim/after
 cp -R ~/.dotfiles/nvim/after ~/.config/nvim
 cp ~/.dotfiles/nvim/init.vim ~/.ideavimrc # Also copy for IntelliJ
+
+# Cargo
+mkdir -p ~/.cargo
+cp ~/.dotfiles/cargo/config.toml ~/.cargo/config.toml
--- scripts/to-dotfiles.sh
+++ scripts/to-dotfiles.sh
@@ -6,5 +6,9 @@ cp ~/.gitconfig ~/.dotfiles/gitconfig
 
 # Neovim
 cp ~/.config/nvim/init.vim ~/.dotfiles/nvim
+cp ~/.config/nvim/coc-settings.json ~/.dotfiles/nvim
 rm -rf ~/.dotfiles/nvim/after
 cp -R ~/.config/nvim/after ~/.dotfiles/nvim
+
+# Cargo
+cp ~/.cargo/config.toml ~/.dotfiles/cargo/config.toml
