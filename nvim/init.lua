d17774924a08af3d6d0ab48df0fc36d0e96d8b66
Max Fang <protonmail.com@maxfa.ng>
2025-07-09 21:08:34 -0700

    vim: Migrate `plug#end()` behavior documentation

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,4 +9,11 @@ vim.cmd('source ' .. config_dir .. '/old-init.vim')
     -- Set up lazy.nvim: lua/config/lazy.lua
     -- TODO(max): Currently not working with fzf
     require("config.lazy")
+
+    -- Note: lazy.nvim automatically executes:
+    -- - `filetype plugin indent on`
+    -- - `syntax enable`
+    -- These can be disabled if needed:
+    -- vim.cmd('filetype off')
+    -- vim.cmd('syntax off')
 --- }
