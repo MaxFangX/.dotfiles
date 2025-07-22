2ebf5801333279711d8afecfc793919c65c1d054
Max Fang <protonmail.com@maxfa.ng>
2025-07-22 16:52:12 -0700

    copilot: Fix keybinding

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -3,15 +3,17 @@ return {
     {
         "github/copilot.vim",
         lazy = false,
+        init = function()
+            -- Disable tab mapping before plugin loads
+            vim.g.copilot_no_tab_map = true
+        end,
         config = function()
             -- Use <Ctrl-Tab> to accept a Copilot suggestion
-            -- Disable tab mapping since it conflicts with CoC autocompletion
             vim.keymap.set('i', '<C-Tab>', 'copilot#Accept("\\<CR>")', {
                 expr = true,
                 replace_keycodes = false,
                 silent = true
             })
-            vim.g.copilot_no_tab_map = true
         end,
     }
 }
