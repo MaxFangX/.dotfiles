360e9e6337ae8aa4789b616ac9b4ddd1073321a3
Max Fang <_@maxfa.ng>
2025-08-29 16:42:25 -0700

    vim: vgit: Don't update vgit upon jump
    This messes up the jump behavior.

--- nvim/lua/plugins/vgit.lua
+++ nvim/lua/plugins/vgit.lua
@@ -291,13 +291,10 @@ return {
       end
       vim.cmd('normal! ' .. target_hunk.start_line .. 'Gzz')
 
-      -- Update quickfix list if it's open
+      -- Update quickfix list highlighting if it's open
       local qf_winid = vim.fn.getqflist({ winid = 0 }).winid
       if qf_winid ~= 0 then
-        -- Refresh the quickfix list to reflect current state
-        helpers.quickfix_unstaged_hunks(false)  -- false = don't log status
-
-        -- Get the updated quickfix list
+        -- Get the current quickfix list (don't refresh)
         local qf_list = vim.fn.getqflist()
 
         -- Find the matching quickfix entry to highlight
