4451d2f5dab643fb88fe9b54ee671482291e41ad
Max Fang <_@maxfa.ng>
2025-10-22 16:29:34 -0700

    scripts: Modernize setup scripts

--- scripts/resource.sh
+++ scripts/resource.sh
@@ -3,15 +3,19 @@ ln -sfF ~/.dotfiles/zshrc ~/.zshrc
 ln -sfF ~/.dotfiles/common ~/.common
 ln -sfF ~/.dotfiles/bashrc ~/.bashrc
 ln -sfF ~/.dotfiles/gitconfig ~/.gitconfig
+ln -sfF ~/.dotfiles/tmux.conf ~/.tmux.conf
 # Neovim
-mkdir -p ~/.config/nvim
-ln -sfF ~/.dotfiles/nvim ~/.config # Works differently when symlinking folders
-ln -sfF ~/.dotfiles/nvim/init.vim ~/.ideavimrc # IntelliJ's copy
+mkdir -p ~/.config
+ln -sfn ~/.dotfiles/nvim ~/.config/nvim
+ln -sfF ~/.dotfiles/nvim/init.lua ~/.ideavimrc # IntelliJ's copy
+# Cargo
+mkdir -p ~/.cargo
+ln -sfF ~/.dotfiles/cargo/config.toml ~/.cargo/config.toml
 # Karabiner
-mkdir -p ~/.config/karabiner/assets/complex_modifications
-ln -sfF \
+mkdir -p ~/.config/karabiner/assets
+ln -sfn \
     ~/.dotfiles/karabiner/assets/complex_modifications \
-    ~/.config/karabiner/assets # Follow the nvim folder example
+    ~/.config/karabiner/assets/complex_modifications
 
 # Backup Karabiner config
 cp ~/.config/karabiner/karabiner.json ~/.dotfiles/karabiner/
