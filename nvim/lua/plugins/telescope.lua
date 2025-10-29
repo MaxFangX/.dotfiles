efd449206a4ce8e02be47ffe6abcf925b18f2398
Max Fang <_@maxfa.ng>
2025-10-28 18:47:34 -0700

    vim: `<Leader>g` to telescope unstaged hunks

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -45,6 +45,44 @@ return {
 
       -- Load fzf extension
       telescope.load_extension("fzf")
+
+      -- Git hunks picker (<Leader>g)
+      vim.keymap.set('n', '<Leader>g', function()
+        local pickers = require("telescope.pickers")
+        local finders = require("telescope.finders")
+        local make_entry = require("telescope.make_entry")
+        local conf = require("telescope.config").values
+
+        local git_hunks = require('git_hunks')
+        local hunks = git_hunks.get_all_hunks()
+
+        if #hunks == 0 then
+          print('No unstaged changes or untracked files found')
+          return
+        end
+
+        -- Convert to quickfix format for telescope
+        local entries = {}
+        for _, hunk in ipairs(hunks) do
+          table.insert(entries, {
+            filename = hunk.file,
+            lnum = hunk.lnum,
+            col = 1,
+            text = hunk.text,
+          })
+        end
+
+        -- Use telescope's built-in quickfix entry maker
+        pickers.new({}, {
+          prompt_title = "Git Hunks",
+          finder = finders.new_table({
+            results = entries,
+            entry_maker = make_entry.gen_from_quickfix({}),
+          }),
+          sorter = conf.generic_sorter({}),
+          previewer = conf.qflist_previewer({}),
+        }):find()
+      end, { noremap = true, silent = true })
     end,
   },
 }
