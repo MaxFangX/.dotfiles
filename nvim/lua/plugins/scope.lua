e503976d0170f0d0bd9e6950f15a5d03c07fbca8
Max Fang <_@maxfa.ng>
2025-10-28 17:02:44 -0700

    vim: Don't close tab if closing last visible buf

--- nvim/lua/plugins/scope.lua
+++ nvim/lua/plugins/scope.lua
@@ -5,5 +5,21 @@ return {
 
     config = function()
         require("scope").setup({})
+
+        -- Smart buffer closing that switches to another buffer instead of
+        -- closing the tab when closing the last visible buffer
+        vim.api.nvim_create_user_command("Bd", function(opts)
+            require("scope.core").close_buffer({
+                force = opts.bang,
+                ask = not opts.bang
+            })
+        end, {
+            bang = true,
+            desc = "Smart close buffer (scope.nvim)"
+        })
+
+        -- Remap :bd to use scope's smart close
+        vim.cmd([[cnoreabbrev bd Bd]])
+        vim.cmd([[cnoreabbrev bd! Bd!]])
     end,
 }
