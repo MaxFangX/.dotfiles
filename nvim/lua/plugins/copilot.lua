92f76091b5dbfc75a4bb209ccae21d94c290cede
Max Fang <_@maxfa.ng>
2025-12-09 20:11:47 -0800

    vim: `<C-r>` to accept Copilot suggestion

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -8,8 +8,8 @@ return {
             vim.g.copilot_no_tab_map = true
         end,
         config = function()
-            -- Use <Ctrl-Tab> to accept a Copilot suggestion
-            vim.keymap.set('i', '<C-Tab>', 'copilot#Accept("\\<CR>")', {
+            -- Use <Ctrl-r> to accept a Copilot suggestion
+            vim.keymap.set('i', '<C-r>', 'copilot#Accept("\\<CR>")', {
                 expr = true,
                 replace_keycodes = false,
                 silent = true
