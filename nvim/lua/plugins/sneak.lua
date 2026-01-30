75062ee6423e0c6f426107081e2b3f4dc1823360
Max Fang <_@maxfa.ng>
2026-01-30 13:59:23 +0900

    vim: sneak: Smart visual options

--- nvim/lua/plugins/sneak.lua
+++ nvim/lua/plugins/sneak.lua
@@ -1,4 +1,16 @@
 -- vim-sneak: Jump to any location with two characters
+
+-- Smart visual s/S: sneak if char/blockwise with no movement, else substitute
+local function smart_visual_s(forward)
+    if vim.fn.mode() == "V" then return "c" end  -- linewise always substitutes
+    local start_pos = vim.fn.getpos("v")
+    local cur_pos = vim.fn.getpos(".")
+    if start_pos[2] == cur_pos[2] and start_pos[3] == cur_pos[3] then
+        return forward and "<Plug>Sneak_s" or "<Plug>Sneak_S"
+    end
+    return "c"
+end
+
 return {
     "justinmk/vim-sneak",
     init = function()
@@ -11,14 +23,14 @@ return {
         -- Respect 'ignorecase' and 'smartcase' settings
         vim.g["sneak#use_ic_scs"] = 1
     end,
-    config = function()
-        -- Remove sneak's 's' mapping in visual mode, restore default behavior
-        pcall(vim.keymap.del, "x", "s")
-    end,
     keys = {
-        -- 2-character Sneak (not in visual mode, where s is substitute)
+        -- 2-character Sneak
         { "s", "<Plug>Sneak_s", mode = { "n", "o" } },
-        { "S", "<Plug>Sneak_S", mode = "n" },
+        { "S", "<Plug>Sneak_S", mode = { "n", "o" } },
+
+        -- Visual: sneak if no selection yet, substitute otherwise
+        { "s", function() return smart_visual_s(true) end, mode = "x", expr = true },
+        { "S", function() return smart_visual_s(false) end, mode = "x", expr = true },
 
         -- Replace f/F with 1-character Sneak (does not invoke label-mode)
         { "f", "<Plug>Sneak_f", mode = { "n", "x", "o" } },
