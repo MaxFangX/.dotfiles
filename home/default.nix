37c6c2a1494809ab924df8083ba2c2ae31cd143f
Max Fang <protonmail.com@maxfa.ng>
2026-02-11 07:37:58 +0000

    nix: Add hetzner host config and omnara module

--- home/default.nix
+++ home/default.nix
@@ -2,13 +2,22 @@
   hm,
   pkgs,
   sources,
+  omnara,
 }:
 {
   max2022 = hm.lib.homeManagerConfiguration {
     pkgs = pkgs;
     modules = [ ./max2022.nix ];
     extraSpecialArgs = {
-      inherit pkgs sources;
+      inherit pkgs sources omnara;
+    };
+  };
+
+  lexe-dev-hetzner = hm.lib.homeManagerConfiguration {
+    pkgs = pkgs;
+    modules = [ ./lexe-dev-hetzner.nix ];
+    extraSpecialArgs = {
+      inherit pkgs sources omnara;
     };
   };
 }
---
+++ home/mods/omnara.nix
@@ -0,0 +1,59 @@
+# Run Omnara as a background daemon service.
+{
+  config,
+  lib,
+  omnara,
+  pkgs,
+  ...
+}:
+let
+  isLinux = pkgs.hostPlatform.isLinux;
+  isDarwin = pkgs.hostPlatform.isDarwin;
+
+  homeDir = config.home.homeDirectory;
+
+  # Run as a login shell so we get normal PATH + aliases
+  execStart = [
+    "${pkgs.bashInteractive}/bin/bash"
+    "-lc"
+    "'exec ${omnara}/bin/omnara daemon run-service'"
+  ];
+in
+{
+  # Add Omnara to PATH
+  home.packages = [ omnara ];
+
+  # Linux - systemd user service
+  systemd.user.services.omnara = lib.mkIf isLinux {
+    Install.WantedBy = [ "default.target" ];
+
+    Unit = {
+      Description = "Omnara Background Daemon";
+      After = [ "network.target" ];
+    };
+
+    Service = {
+      Type = "simple";
+      ExecStart = builtins.concatStringsSep " " execStart;
+      Restart = "on-failure";
+      RestartSec = 5;
+      WorkingDirectory = homeDir;
+    };
+  };
+
+  # macOS - launchd agent
+  launchd.agents.omnara = lib.mkIf isDarwin {
+    enable = true;
+    config = {
+      ProcessType = "Background";
+      ProgramArguments = execStart;
+      WorkingDirectory = homeDir;
+      RunAtLoad = true;
+      KeepAlive = {
+        SuccessfulExit = false;
+        Crashed = true;
+      };
+      ThrottleInterval = 5;
+    };
+  };
+}
