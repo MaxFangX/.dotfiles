c49c2e7497e294322af89be611d6f9046f4ff9dc
Max Fang <_@maxfa.ng>
2026-04-14 15:58:41 -0700

    nvim: Silence sneak#wrap command echo

--- nvim/lua/plugins/sneak.lua
+++ nvim/lua/plugins/sneak.lua
@@ -19,11 +19,11 @@ return {
 
         -- 1-character Sneak with label-mode for f/F
         -- Use :<c-u> instead of <Cmd> to properly clear state between invocations
-        { "f", ":<c-u>call sneak#wrap('', 1, 0, 1, 1)<CR>", mode = "n" },
-        { "F", ":<c-u>call sneak#wrap('', 1, 1, 1, 1)<CR>", mode = "n" },
-        { "f", ":<c-u>call sneak#wrap(visualmode(), 1, 0, 1, 1)<CR>", mode = "x" },
-        { "F", ":<c-u>call sneak#wrap(visualmode(), 1, 1, 1, 1)<CR>", mode = "x" },
-        { "f", ":<c-u>call sneak#wrap(v:operator, 1, 0, 1, 1)<CR>", mode = "o" },
-        { "F", ":<c-u>call sneak#wrap(v:operator, 1, 1, 1, 1)<CR>", mode = "o" },
+        { "f", ":<c-u>silent call sneak#wrap('', 1, 0, 1, 1)<CR>", mode = "n" },
+        { "F", ":<c-u>silent call sneak#wrap('', 1, 1, 1, 1)<CR>", mode = "n" },
+        { "f", ":<c-u>silent call sneak#wrap(visualmode(), 1, 0, 1, 1)<CR>", mode = "x" },
+        { "F", ":<c-u>silent call sneak#wrap(visualmode(), 1, 1, 1, 1)<CR>", mode = "x" },
+        { "f", ":<c-u>silent call sneak#wrap(v:operator, 1, 0, 1, 1)<CR>", mode = "o" },
+        { "F", ":<c-u>silent call sneak#wrap(v:operator, 1, 1, 1, 1)<CR>", mode = "o" },
     },
 }
