01e8bf75fae4cbddd849b76e70d11b3e6c6e9658
Max Fang <hello@maxfa.ng>
2026-04-16 12:28:32 -0700

    home/nitropad: Configure alacritty

--- home/max-nitropad-2024.nix
+++ home/max-nitropad-2024.nix
@@ -5,6 +5,24 @@
     ./mods/core.nix
   ];
 
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      window.dimensions = {
+        columns = 103;
+        lines = 19;
+      };
+
+      keyboard.bindings = [
+        {
+          key = "N";
+          mods = "Control|Shift";
+          action = "CreateNewWindow";
+        }
+      ];
+    };
+  };
+
   home.username = "max";
   home.homeDirectory = "/home/max";
   home.stateVersion = "25.05";
