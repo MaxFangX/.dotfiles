e4f5f4e7b520913473b88652c000b738ecf248f9
Max Fang <_@maxfa.ng>
2026-04-24 12:25:17 -0700

    vim: Disable sneak's `f/F`

--- nvim/lua/plugins/sneak.lua
+++ nvim/lua/plugins/sneak.lua
@@ -11,18 +11,26 @@ return {
 
         -- Respect 'ignorecase' and 'smartcase' settings
         vim.g["sneak#use_ic_scs"] = 1
+
+        -- Disable f/t reset: sneak#reset() unmaps f/t even when sneak didn't
+        -- create the mapping, which clears our DVORAK t→k mapping.
+        vim.g["sneak#t_reset"] = 0
+        vim.g["sneak#f_reset"] = 0
     end,
     keys = {
         -- 2-character Sneak (visual surround is R, see tpope.lua)
         { "s", "<Plug>Sneak_s", mode = { "n", "o", "x" } },
         { "S", "<Plug>Sneak_S", mode = { "n", "o", "x" } },
 
-        -- 1-character Sneak with label-mode for f/F
-        -- Use :<c-u> instead of <Cmd> to properly clear state between invocations
+        -- 1-character Sneak with label-mode for f/F (normal and operator-pending)
+        --
+        -- NOTE: We don't use sneak's f/F because it consumes the `R` keypress
+        -- that we use to trigger vim-surround when we have text selected, even
+        -- if we configure sneak's labels to exclude 'R'.
         { "f", ":<c-u>silent call sneak#wrap('', 1, 0, 1, 1)<CR>", mode = "n" },
         { "F", ":<c-u>silent call sneak#wrap('', 1, 1, 1, 1)<CR>", mode = "n" },
-        { "f", ":<c-u>silent call sneak#wrap(visualmode(), 1, 0, 1, 1)<CR>", mode = "x" },
-        { "F", ":<c-u>silent call sneak#wrap(visualmode(), 1, 1, 1, 1)<CR>", mode = "x" },
+        -- { "f", ":<c-u>silent call sneak#wrap(visualmode(), 1, 0, 1, 1)<CR>", mode = "x" },
+        -- { "F", ":<c-u>silent call sneak#wrap(visualmode(), 1, 1, 1, 1)<CR>", mode = "x" },
         { "f", ":<c-u>silent call sneak#wrap(v:operator, 1, 0, 1, 1)<CR>", mode = "o" },
         { "F", ":<c-u>silent call sneak#wrap(v:operator, 1, 1, 1, 1)<CR>", mode = "o" },
     },
